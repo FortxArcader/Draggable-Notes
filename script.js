ce06a528e330c5adb82c69cc5e8db478407b5a90
Shlok Bhardwaj <shlokbhardwaj1@gmail.com>
2024-08-13 13:07:05 +0530

    update for mobile dragging and touch

--- script.js
+++ script.js
@@ -3,39 +3,51 @@ document.addEventListener('DOMContentLoaded', () => {
     const notes = document.querySelectorAll('.note');
 
     notes.forEach(note => {
-        note.addEventListener('mousedown', (e) => {
-            let shiftX = e.clientX - note.getBoundingClientRect().left;
-            let shiftY = e.clientY - note.getBoundingClientRect().top;
+        const startDragging = (e) => {
+            e.preventDefault();
+            const clientX = e.type === 'touchstart' ? e.touches[0].clientX : e.clientX;
+            const clientY = e.type === 'touchstart' ? e.touches[0].clientY : e.clientY;
+            
+            let shiftX = clientX - note.getBoundingClientRect().left;
+            let shiftY = clientY - note.getBoundingClientRect().top;
 
             function moveAt(pageX, pageY) {
                 note.style.left = pageX - shiftX + 'px';
                 note.style.top = pageY - shiftY + 'px';
             }
 
-            moveAt(e.pageX, e.pageY);
+            moveAt(clientX, clientY);
 
-            function onMouseMove(event) {
-                moveAt(event.pageX, event.pageY);
+            function onMove(event) {
+                const clientX = event.type === 'touchmove' ? event.touches[0].clientX : event.clientX;
+                const clientY = event.type === 'touchmove' ? event.touches[0].clientY : event.clientY;
+                moveAt(clientX, clientY);
             }
 
-            document.addEventListener('mousemove', onMouseMove);
+            document.addEventListener('mousemove', onMove);
+            document.addEventListener('touchmove', onMove);
 
-            note.addEventListener('mouseup', () => {
-                document.removeEventListener('mousemove', onMouseMove);
-            });
+            function stopDragging() {
+                document.removeEventListener('mousemove', onMove);
+                document.removeEventListener('touchmove', onMove);
+                document.removeEventListener('mouseup', stopDragging);
+                document.removeEventListener('touchend', stopDragging);
+            }
 
-            note.addEventListener('mouseleave', () => {
-                document.removeEventListener('mousemove', onMouseMove);
-            });
+            document.addEventListener('mouseup', stopDragging);
+            document.addEventListener('touchend', stopDragging);
+        };
 
-            note.ondragstart = () => false;
-        });
+        note.addEventListener('mousedown', startDragging);
+        note.addEventListener('touchstart', startDragging);
+        
+        note.ondragstart = () => false; 
     });
 
-    // Heart Falling Animation
+  
     const heartsContainer = document.getElementById('hearts-container');
 
-    for (let i = 0; i < 20; i++) { // You can adjust the number of hearts
+    for (let i = 0; i < 20; i++) { 
         const heart = document.createElement('div');
         heart.classList.add('heart');
         heart.style.left = Math.random() * 100 + 'vw';
@@ -45,5 +57,5 @@ document.addEventListener('DOMContentLoaded', () => {
 
     setTimeout(() => {
         heartsContainer.classList.add('hide-hearts');
-    }, 5000); // Remove hearts after 5 seconds
+    }, 5000); 
 });
